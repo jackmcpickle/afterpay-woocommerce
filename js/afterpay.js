90f9169a870d1c8341e12c402b6a7ee37144fc12
Jack McNicol <jack@mcpickle.com.au>
2018-12-29 10:48:11 +1030

    Move afterpay modal call into mainjs
    listen on the document, attach to the element.

--- js/afterpay.js
+++ js/afterpay.js
@@ -1,18 +1,19 @@
 jQuery(document).ready(function($) {
-	
-	checkout_payment_type();
 
-	function checkout_payment_type() {
-		$('input[type="radio"][name="afterpay_payment_type"]').on('change',function() {
-			if ($('input[type="radio"][name="afterpay_payment_type"]:checked').val() == "PAD") {
-				$('.afterpay_pad_description').slideDown(300);
-				$('.afterpay_pbi_description').slideUp(300);
-			} else {
-				$('.afterpay_pad_description').slideUp(300);
-				$('.afterpay_pbi_description').slideDown(300);
-			}
-		});
+    $(document).on('change', 'input[name="afterpay_payment_type"]', function() {
+        if ($('input[type="radio"][name="afterpay_payment_type"]:checked').val() == 'PAD') {
+            $('.afterpay_pad_description').slideDown(300);
+            $('.afterpay_pbi_description').slideUp(300);
+        } else {
+            $('.afterpay_pad_description').slideUp(300);
+            $('.afterpay_pbi_description').slideDown(300);
+        }
+    });
 
-		$('input[name="afterpay_payment_type"]').trigger('change');
-	}
+    $('input[name="afterpay_payment_type"]').trigger('change');
+
+    $(document).on('click', '#what-is-afterpay-trigger', function(e) {
+        e.preventDefault();
+        jQuery(e.currentTarget).fancybox();
+    });
 });
